0a83ebc8f0050748111eff1bf6aa97899f300ff8
xChronos11 <60798570+xChronos11@users.noreply.github.com>
2020-10-09 19:07:05 -0700

    Second commit 5-8 Exercises

--- Practica_2.scala
+++ Practica_2.scala
@@ -10,3 +10,13 @@ val lista = List("rojo","blanco","negro","verde","amarillo","azul","naranja","pe
 lista slice (3, 6)
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
 Array.range(1, 1000, 5)
+
+// 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
+val mylist = List(1,3,3,4,6,7,3,7)  
+val newset = mylist.toSet
+// 6. Crea una mapa mutable llamado nombres que contenga los siguiente "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
+val mutmap = collection.mutable.Map(("Jose", 20), ("Luis", 24), ("Ana", 23), ("Susana", 27))
+// 6 a . Imprime todas la llaves del mapa
+mutmap.keys
+// 7 b . Agrega el siguiente valor al mapa("Miguel", 23)
+mutmap += ("Miguel" -> 23)
