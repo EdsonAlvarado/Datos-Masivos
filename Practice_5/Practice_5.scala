be51d26858339effdc7c8abb47848a2b523f0392
FRANCISCO DIAZ <zightguitar@gmail.com>
2020-10-15 20:00:50 -0700

    Second commit, second half of exercises

--- Practice_5/Practice_5.scala
+++ Practice_5/Practice_5.scala
@@ -4,26 +4,47 @@ val spark = SparkSession.builder().getOrCreate()
 
 val df = spark.read.option("header", "true").option("inferSchema","true")csv("CitiGroup2006_2008")
 
-//Returns the first n rows in the dataset
+//1 Returns the first n rows in the dataset
 df.take(n)
 
-//Computes statistics for columns, example df.summary().show()
+//2 Computes statistics for columns, example df.summary().show()
 df.summary()
 
-//Returns rows as list
+//3 Returns rows as list
 df.takeAsList(n)
 
-//returns an array of the dataset columns
+//4 Returns an array of the dataset columns
 df.columns
 
-//returns an array of the column names an their data types
+//5 Returns an array of the column names an their data types
 df.dtypes
 
-//Returns true if the dataset is empty
+//6 Returns true if the dataset is empty
 df.isEmpty
 
-//returns the unique rows of the dataset
+//7 Returns the unique rows of the dataset
 df.distinct.show()
 
-//returns a new dataset with the first n rows, example: val df2 = df.limit(10) 
+//8 Returns a new dataset with the first n rows, example: val df2 = df.limit(10) 
 df.limit(n)
+
+//9 Prints the plans (logical and physical) to the console for debugging purposes.
+df.explain()
+
+//10 Order by the column selected.
+df.orderBy("Close").show
+
+//11 Get the variance of the column.
+df.select(variance("Low")).show()
+
+//12 Get the min value of the column High.
+df.select(min("High")).show
+
+//13 Get the standard deviation.
+df.select(stddev("High")).show()
+
+//14 Prints the schema to the console in a nice tree format.
+df.printSchema()
+
+//15 Get the kurtosis of the column.
+df.select(kurtosis("High")).show()
