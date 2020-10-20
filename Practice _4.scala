362dd9ce1eebb2420b5a7fcf9511c46b3a2acb85
FRANCISCO DIAZ <zightguitar@gmail.com>
2020-10-19 18:48:24 -0700

    Fix Ecercise 3 & 4

--- Practice _4.scala
+++ Practice _4.scala
@@ -17,21 +17,21 @@ import scala.math.sqrt
 def fib2(n:Double):Double={
     
     if(n<2){
-    return n}
+    return n} else{
     var i: Double = ((1 + Math.sqrt(5)) / 2)
     var j: Double = 0
      j = ((Math.pow(i, n) - Math.pow((1 - i), n)) / Math.sqrt(5))
         return j   
     }   
-{ 
+}
 
 //3
+
+def fib3(n: Int): Int = {
 var a: Int = 0
 var b: Int = 1
 var c: Int = 0
 
-def fib3(n: Int): Int = {
-  
     for (k <- Range(0, n)){
         c = b + a
         a = b
@@ -42,10 +42,12 @@ def fib3(n: Int): Int = {
 }
 
 //4
+
+def fib4(n: Int): Int = {
+
 var d: Int = 0
 var e: Int = 1
 
-def fib4(n: Int): Int = {
   
     for (k <- Range(0, n)){
         e = e + d
