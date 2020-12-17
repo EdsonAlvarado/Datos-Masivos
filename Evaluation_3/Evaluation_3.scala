647be0bb348d03c7937a397304744ec6bfba9a14
EdsonAlvarado <60414153+EdsonAlvarado@users.noreply.github.com>
2020-12-16 20:25:54 -0800

    Evaluation_3 second part

--- Evaluation_3/Evaluation_3.scala
+++ Evaluation_3/Evaluation_3.scala
@@ -16,3 +16,26 @@ val dataset = spark.read.option("header","true").option("inferSchema","true").cs
 
 // 6 Seleccione las siguientes columnas: Fresh, Milk, Grocery, Frozen, Detergents_Paper, Delicassen y llamar a este conjunto feature_data
 val feature_data = dataset.select("Fresh","Milk","Grocery","Frozen","Detergents_Paper","Delicassen")
+
+// 7 Import vector assambler and vector
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+
+// 8 Crea un nuevo objeto Vector Assembler para las columnas de caracteristicas como un conjunto de entrada, recordando que no hay etiquetas
+val assembler = new VectorAssembler().setInputCols(Array("Fresh", "Milk", "Grocery", "Frozen","Detergents_Paper","Delicassen")).setOutputCol("features")
+
+// 9 Utilice el objeto assembler para transformar feature_data
+val features = assembler.transform(feature_data)
+features.show()
+
+// 10 Crear un modelo Kmeans con K=3
+val kmeans = new KMeans().setK(3).setSeed(1L)
+val model = kmeans.fit(features)
+
+// 11 Evalúe los grupos utilizando Within Set Sum of Squared Errors WSSSE e imprima los centroides
+val WSSSE = model.computeCost(features)
+println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+// Mostrar resultados
+println("Cluster Centers: ")
+model.clusterCenters.foreach(println)
